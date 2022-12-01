181e7fae077cb6cda57c8ed61188a0b58e6eed61
LoBBiTo <pablotrubbo@gmail.com>
2022-12-01 00:42:48 -0300

    first commit

---
+++ Clicky.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "Me gusta la papa"
+    let b = miString + " pero mas el pure"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "Dame papaaas"
+
+    console.log(a)
+}
